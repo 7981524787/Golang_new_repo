a14572d8795a2a4752ec8b57c5342ccf97d9eb88
Jiten <jpalaparthi@gmail.com>
2024-11-28 13:21:23 +0530

    add counter example on mutex

--- 35-goroutine-mutex/main.go
+++ 35-goroutine-mutex/main.go
@@ -3,6 +3,7 @@ package main
 import "sync"
 
 var counter int = 0
+var mu *sync.Mutex = new(sync.Mutex)
 
 func main() {
 	wg := new(sync.WaitGroup)
@@ -10,29 +11,58 @@ func main() {
 
 	go func() {
 		for i := 1; i <= 200; i++ {
-			go incr(wg)
+			go incr(wg, mu)
 		}
 		wg.Done()
 	}()
 
 	go func() {
 		for i := 1; i <= 100; i++ {
-			go decr(wg)
+			go decr(wg, mu)
 		}
 		wg.Done()
 	}()
-	println(counter)
+
 	wg.Wait()
+	println(counter)
+
 }
 
-func incr(wg *sync.WaitGroup) {
+func incr(wg *sync.WaitGroup, mu *sync.Mutex) {
+	mu.Lock()
 	counter++
+	mu.Unlock()
 	wg.Done()
 }
 
-func decr(wg *sync.WaitGroup) {
+func decr(wg *sync.WaitGroup, mu *sync.Mutex) {
+	mu.Lock()
 	counter--
+	mu.Unlock()
 	wg.Done()
 }
 
 // do not sharem memory by communicating, do communicate by sharing memory
+
+type Counter struct {
+	C  int
+	Mu *sync.Mutex
+}
+
+func New(mu *sync.Mutex, c int) *Counter {
+	return &Counter{C: c, Mu: mu}
+}
+
+func (c *Counter) Incr(wg *sync.WaitGroup, mu *sync.Mutex) {
+	c.Mu.Lock()
+	c.C++
+	c.Mu.Unlock()
+	wg.Done()
+}
+
+func (c *Counter) Decr(wg *sync.WaitGroup, mu *sync.Mutex) {
+	c.Mu.Lock()
+	c.C--
+	c.Mu.Unlock()
+	wg.Done()
+}
