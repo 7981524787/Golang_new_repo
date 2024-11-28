f89aed1a4d950aec0d5ba674f27653026baf99a8
Jiten <jpalaparthi@gmail.com>
2024-11-28 10:49:54 +0530

    add more interfaces

---
+++ 29-interface/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	r1 := New(123.123, 123.23)
+	var s1 Square = 123.123
+	Shape(r1)
+	Shape(s1)
+}
+
+func Shape(ishape IShape) {
+	fmt.Println("Area of:", ishape.What(), "is:", ishape.Area())
+	fmt.Println("Perimeter of:", ishape.What(), "is:", ishape.Perimeter())
+}
+
+type IShape interface {
+	// Area() float32
+	// Perimeter() float32
+	IArea
+	IPerimeter
+	IWhat
+}
+
+type IArea interface {
+	Area() float32
+}
+
+type IPerimeter interface {
+	Perimeter() float32
+}
+
+type IWhat interface {
+	What() string
+}
+
+type Rect struct {
+	L    float32
+	B    float32
+	A, P float32
+}
+
+func New(l, b float32) *Rect {
+	return &Rect{L: l, B: b}
+}
+
+func (r *Rect) Area() float32 { // function
+	(*r).A = (*r).L * r.B
+	return r.A
+}
+
+func (r *Rect) Perimeter() float32 { // function
+	r.P = 2 * (r.L + r.B)
+	return r.P
+}
+
+func (r *Rect) What() string {
+	return "Rect"
+}
+
+type Square float32
+
+func (s Square) Area() float32 { // function
+	return float32(s * s)
+}
+
+func (s Square) Perimeter() float32 { // function
+	return float32(4 * s)
+}
+
+func (s Square) What() string {
+	return "Square"
+}
---
+++ 30-interfaces-filewriter/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fw := NewFile("data.txt")
+
+	fmt.Fprintln(fw, "Hello World")
+	fmt.Fprintln(os.Stdout, "Hello World")
+	ew := &EmailWriter{sender: "jp@outlook.com", receiver: "somereceiver@gmail.com", email: "dummy styff"}
+	fmt.Fprintln(ew, "Hello World")
+
+}
+
+type EmailWriter struct {
+	email    string
+	sender   string
+	receiver string
+}
+
+func (fw *EmailWriter) Write(p []byte) (n int, err error) {
+	//
+	return 0, nil
+}
+
+type FileWriter struct {
+	Filename string
+}
+
+func NewFile(fn string) *FileWriter {
+	return &FileWriter{Filename: fn}
+}
+
+func (fw *FileWriter) Write(p []byte) (n int, err error) {
+	f, err := os.OpenFile(fw.Filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close() // ignore defer as of now
+
+	return f.Write(p)
+}
+
+type IShape interface {
+	Area() float32
+	Perimeter() float32
+}
+
+type Rect struct {
+	L    float32
+	B    float32
+	A, P float32
+}
+
+func New(l, b float32) *Rect {
+	return &Rect{L: l, B: b}
+}
+
+func (r *Rect) Area() float32 { // function
+	(*r).A = (*r).L * r.B
+	return r.A
+}
+
+func (r *Rect) Perimeter() float32 { // function
+	r.P = 2 * (r.L + r.B)
+	return r.P
+}
+
+func (r *Rect) What() string {
+	return "Rect"
+}
