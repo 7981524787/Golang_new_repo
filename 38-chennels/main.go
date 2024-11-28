db3f8685067c4572c449ffafdc764db8408004c7
Jiten <jpalaparthi@gmail.com>
2024-11-28 15:51:17 +0530

    add more exampels on channels

--- 38-chennels/main.go
+++ 38-chennels/main.go
@@ -2,47 +2,43 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"time"
 )
 
 func main() {
-	ch := make(chan int)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 	sig1 := make(chan struct{})
 	sig2 := make(chan empty)
-	go generate(100, ch)
-	go receiver1(ch, sig1)
-	go receiver2(ch, sig2)
-	// v := <-sig
-	// fmt.Println(v)
+	go generate(100, ch1)
+	go receiver1(ch1, ch2, sig1)
+	go receiver2(ch2, sig2)
+
 	<-sig1
 	<-sig2
 }
 
-func generate(num int, ch chan int) {
+func generate(num int, ch1 chan int) {
 	for i := 1; i <= num; i++ {
-		time.Sleep(time.Millisecond * 200)
-		ch <- i * i
+		ch1 <- i
 	}
-	close(ch) // closing channel is a pattern
+	close(ch1) // closing channel is a pattern
 }
-func receiver1(ch chan int, sig chan struct{}) {
-	for {
-		v, ok := <-ch
-		if !ok {
-			sig <- struct{}{}
-			runtime.Goexit()
-		} else {
-			fmt.Println("receiver-1", v)
-		}
+func receiver1(ch1 chan int, ch2 chan int, sig chan struct{}) {
+	for v := range ch1 {
+		fmt.Println("Received from Generator-->", v)
+		ch2 <- v * v
 	}
+	close(ch2)
+	sig <- struct{}{}
+	close(sig)
 }
 
-func receiver2(ch chan int, sig chan empty) {
-	for v := range ch {
-		fmt.Println("receiver-2", v)
+func receiver2(ch2 chan int, sig chan empty) {
+	for v := range ch2 {
+		fmt.Println("Received from receiver1-->", v)
 	}
 	sig <- empty{}
+	close(sig)
 }
 
 type empty struct{}
